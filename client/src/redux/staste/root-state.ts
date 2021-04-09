0d6db41b69f5973e89e35a32ecf5770b15c0f256
winimus <minimus@snu.ac.kr>
2021-04-09 14:31:33 +0900

    state spelling error

--- client/src/redux/staste/root-state.ts
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { AssertsIdentifierTypePredicate } from "typescript";
-
-export interface RootState {}
-
-export function useRootSelector<T>(selector: (state: RootState) => T): T {
-  return useSelector(selector);
-}
