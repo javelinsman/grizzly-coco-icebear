3071d456a688dd5f78a0e2bd1cab908a5208af62
winimus <minimus@snu.ac.kr>
2021-04-16 15:29:58 +0900

    update

--- data_generation.py
+++ data_generation.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 def generate_patient() :
     patients = []
-    years = ["88", "89", "90", "91", "92"]
+    years = ["85", "86", "87", "88", "89", "90", "91", "92", "93", "94"]
     months = [f"{m:02d}" for m in range(1, 13)]
     days = [f"{d:02d}" for d in range(1, 28)]
     
@@ -13,10 +13,11 @@ def generate_patient() :
         pid = random.choice(years)+random.choice(months)+random.choice(days)
         gender = 'female'
         age = str(122 - int(pid[0:2]))
-        preg_data = datetime(2020, random.randrange(1, 13), random.randrange(1, 28)).strftime("%Y-%m-%d")
+        preg_date = datetime(2020, random.randrange(1, 13), random.randrange(1, 28)).strftime("%Y-%m-%d")
         height = str(random.randrange(155, 175))
         weight = str(random.randrange(50, 70))
-        patients.append({'patient_id': pid, 'gender': gender, 'age': age, 'preg_date': preg_data, 'weight': weight, 'height': height})
+        p_type = str(random.randrange(0, 4))
+        patients.append({'patient_id': pid, 'p_type': p_type, 'gender': gender, 'age': age, 'preg_date': preg_date, 'weight': weight, 'height': height})
     return patients
 # 88 ~ 92
 # 01 ~ 12
@@ -54,6 +55,19 @@ def generate_bs(type) :
     # type2: 75 +- 25   100 +- 25
     # type3: 85 +- 15   110 +- 15
     # type4: 85 +- 25   110 +- 25
+    ebs_base = 75
+    abs_base = 100
+    bs_range = 15
+    
+    if type == 1 :
+        bs_range = 25
+    elif type == 2 :
+        ebs_base = 85
+        abs_base = 110
+    elif type == 3 :
+        ebs_base = 85
+        abs_base = 110
+        bs_range = 25
 
     return []
 
@@ -61,25 +75,34 @@ def generate_bw(type) :
     # body weight 
     # ~ week 20 0.32kg / week
     # ~ week 40 0.45kg / week
-    # type1 : 0.32 +- -0.03~0.03 0.45 +- -0.04~0.04
-    # type2 : 0.32 +-  -0.015~0.045 +- -0.02~0.06
+    # type1 : 0.32 +- -0.032~0.032 0.45 +- -0.045~0.045
+    # type2 : 0.32 +-  -0.00~0.0.32 +- 0.045
+    # type3 : 0.32 +- -0.032 ~ 00   0.45 += 0.045 
+
+    bw_20 = 0.32
+    bw_40 = 0.45
+    bw_20_range = 0.016
+    bw_40_range = 0.022
+    bw_20_base = 0
+    bw_40_base = 0
+
+    if type == 1 :
+        bw_20_base = 0.016
+        bw_40_base = 0.022
+
+    if type == 2 :
+        bw_20_base = -0.016
+        bw_40_base = -0.022
 
     return 
 def generate_data(patient) :
-    # blood pressure: pid, timetamp, sbp, dbp   3 / day
+    # blood pressure: pid, timetamp, sbp, dbp   1 / day
     # body weight:  pid, timetamp, value 1 / week
-    # blood sugar: pid, timetamp, type, value   3 / day
+    # blood sugar: pid, timetamp, type, value   1 / day
     print(patient)
     begin_date = datetime.strptime(patient['preg_date'], "%Y-%m-%d")
 
-    bp_type = random.randrange(0, 4)
-    bs_type = random.randrange(0, 4)
-    bp_type = random.randrange(0, 2)
-   
-    
-   
 
-   
     return []
 def run() :
     #patients = generate_patient()
